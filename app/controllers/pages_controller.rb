8362d1cc9e8df33682ee3edbb5e6743a2dfb4ea3
Diane <d.bunod@gmail.com>
2019-01-17 13:47:43 -0800

    v40

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,14 @@ class PagesController < ApplicationController
     @users = User.all
     current_user
     @partners = Partner.all
+    @partners_startups = []
+    @partners_gg = []
+    @partners.each do |partner|
+      if partner.grand_groupe == true
+        then @partners_gg << partner
+      else @partners_startups << partner
+      end
+    end
   end
 
   def startups
